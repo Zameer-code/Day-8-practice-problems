a2b0f79f91d19941a7e9a6c7e43833c677fe5ea1
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-07 16:26:52 +0530

    Add files via upload

---
+++ EmpWageOopsConcept/src/main/java/com/mpageOopsoncept/EmployeeWageforMultipleCompanies.java
@@ -0,0 +1,48 @@
+package com.employeewage;
+
+public class EmployeeWageforMultipleCompanies {
+		
+	/** UC8 Calculating Employee Wage for differernt companies **/
+	
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+	
+	public static int calcEmplWageForCompany(String company, int empRate, int numOfDays, int maxHrs) {
+	
+		// Variables
+				int empHrs = 0,	totalEmpHrs = 0,	totalWorkingDays = 0;
+				// Computation
+				while (totalEmpHrs <= maxHrs &&
+						totalWorkingDays < numOfDays) {
+					totalWorkingDays++;
+					int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+					switch (empCheck) {
+						case IS_PART_TIME:
+							empHrs = 4;
+							break;
+						case IS_FULL_TIME:
+							empHrs = 8;
+							break;
+						default:
+							empHrs = 0;
+					}
+					totalEmpHrs += empHrs;
+					System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " +empHrs);	
+				}
+				int totalEmpWage = totalEmpHrs * empRate;
+				System.out.println("Total Emp Wage for Company: " +company+" is: "+ totalEmpWage);
+				return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		calcEmplWageForCompany("DMart", 20, 2, 10);
+		calcEmplWageForCompany("Reliance", 10, 4, 20);
+		
+	}
+}
+
+
+	
+
